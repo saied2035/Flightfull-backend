99fefe5a52eea30c0a69c314edaaf4f3a7b00a47
Saied Gaffer <saied2421998@gmail.com>
2022-07-18 11:49:41 +0200

    modify associations. user has items and reserved items. item belongs to user and has reserving users

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_many :reservations, dependent: :destroy
+  has_many :reserving_users, through: :reservations, source: :user
 end
--- db/migrate/20220717092035_create_items.rb
+++ db/migrate/20220717092035_create_items.rb
@@ -4,11 +4,8 @@ class CreateItems < ActiveRecord::Migration[7.0]
       t.string :name
       t.integer :flight_number
       t.string :image
-      t.timestamps :departure_time
       t.integer :price
-      t.boolean :reserved, default: false
       t.references :user, null: false, foreign_key: true
-
       t.timestamps
     end
   end
