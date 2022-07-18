554d9125ddbdfe7cb2a8746a0537e4826624cd1d
Saied Gaffer <saied2421998@gmail.com>
2022-07-18 16:48:34 +0200

    finish reserve a flight api end point

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -9,5 +9,21 @@ class Api::V1::ReservationsController < ApplicationController
 	    else
 	      render json: {Error: 'Devise gem hasn\'t been set up yet.'}
 	    end	
+	end
+
+	def create
+		user = User.find(params[:user_id])
+        item = Item.find(params[:item_id])
+        reservation = Reservation.new(city:params[:city],date:Date.parse(params[:date]),user:user,item:item)
+        if reservation.save
+          render json: {success: "You reserved #{item.name} on #{reservation.date} to #{reservation.city}."}.to_json
+        else
+          render json: {failure: "Something went wrong. Please, try again."}.to_json	
+        end
+	end
+
+	private
+	def reservation_params
+		params.require(:reservation).permit(:city,:date,:user_id,:item_id)
 	end	
 end
