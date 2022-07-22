159a2da7ee120e5f1210a9ea675e938ebcdf307d
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-07-22 22:22:20 +0430

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
      namespace :api, defaults: { format: 'json' } do
       namespace :v1, defaults: { format: 'json' } do
+        post 'login', to: 'auth#login'
+        get 'auth', to: 'auth#persist'
         resources :reservations, only: [:index, :create, :destroy]
         resources :users
         resources :items, only: [:index, :create, :update, :destroy]
