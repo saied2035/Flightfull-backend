345dfed920f79cddde89112c0561f4b983f7e666
nevisende <ekonomitakipmail@gmail.com>
2022-07-26 13:57:43 +0300

    Feat: add validations of item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :reservations, dependent: :destroy
   has_many :reserving_users, through: :reservations, source: :user
+  validates :name, :image, presence: true
+  validates :flight_number, numericality: true
+  validates :price, numericality: true
 end
